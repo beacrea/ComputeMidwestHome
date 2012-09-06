bd63256e65064a06e1597831ea247aa2b70e0c05
Coty Beasley <design@beasleycreative.com>
2012-09-06 04:00:16 -0500

    Revert!

--- js/main.js
+++ js/main.js
@@ -1,25 +1,8 @@
 /* Author:
 
 */
+$(function(){
+  $('h1').fitText(3, {minFontSize:20});
+  $('p') .fitText(4, {minFontSize:14});
 
-$('#about h1').fitText(1, {minFontSize:20});
-$('#about p') .fitText(2, {minFontSize:14});
-
-$aboutBg = $('#about .bg');
-$aboutBgParent = $aboutBg.parent();
-
-function scaleBg(){
-  var pW = $aboutBgParent.outerWidth()
-    , w  = $aboutBg.height()
-  ;//var
-
-  $aboutBg.width(pW);
-
-  var pH = $aboutBgParent.outerHeight()
-    , h  = $aboutBg.height()
-  ;//var
-
-  pH > h && $aboutBg.height(pH);
-}
-scaleBg();
-$(window).resize(scaleBg);
+});
